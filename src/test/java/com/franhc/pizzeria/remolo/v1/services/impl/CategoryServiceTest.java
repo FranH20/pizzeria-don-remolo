18d5eb6ed2f94acf1ba993c022bb31de47eac4aa
Franklin Huichi <carloshuichi@gmail.com>
2023-05-03 21:06:52 -0500

    Agregando el test de getCategories

--- src/test/java/com/franhc/pizzeria/remolo/v1/services/impl/CategoryServiceTest.java
+++ src/test/java/com/franhc/pizzeria/remolo/v1/services/impl/CategoryServiceTest.java
@@ -4,21 +4,22 @@ import com.franhc.pizzeria.remolo.v1.models.Category;
 import com.franhc.pizzeria.remolo.v1.models.Subcategory;
 import com.franhc.pizzeria.remolo.v1.payloads.requests.CategoryRequest;
 import com.franhc.pizzeria.remolo.v1.payloads.responses.CategoryResponse;
+import com.franhc.pizzeria.remolo.v1.payloads.responses.pagination.PaginationResponse;
 import com.franhc.pizzeria.remolo.v1.repositories.CategoryRepository;
 import com.franhc.pizzeria.remolo.v1.repositories.SubcategoryRepository;
 import com.franhc.pizzeria.remolo.v1.util.ObjectMapperReadJson;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
 import java.io.IOException;
 import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class CategoryServiceTest {
@@ -42,5 +43,13 @@ class CategoryServiceTest {
         assertNotNull(categoryResponse);
     }
 
-
+    @Test
+    void getCategories() throws IOException {
+        Category category = ObjectMapperReadJson.INSTANCE.getCategoryDatabase();
+        when(categoryRepository.findAll(any(PageRequest.class))).thenReturn(new PageImpl<>(Collections.singletonList(category)));
+        PaginationResponse<CategoryResponse> response = categoryService.getCategories(0, 10);
+        assertNotNull(response);
+        assertNotNull(response.getData());
+        assertNotNull(response.getPagination());
+    }
 }
